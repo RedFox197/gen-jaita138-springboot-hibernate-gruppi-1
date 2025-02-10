45de8f3d7bcc4a111fed01364aa00353bf2dce10
Leonardo Boscolo Boscoletto <boscololeo97@gmail.com>
2025-02-10 16:21:14 +0100

    Implementazione Genere

--- src/main/java/com/github/redfox197/demo/database/entity/Genere.java
+++ src/main/java/com/github/redfox197/demo/database/entity/Genere.java
@@ -1,5 +1,55 @@
 package com.github.redfox197.demo.database.entity;
 
+import java.util.List;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity // nome
 public class Genere {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String nome;
+    @ManyToMany
+    private List<Libro> libri;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Libro> getLibri() {
+        return libri;
+    }
+
+    public void setLibri(List<Libro> libri) {
+        this.libri = libri;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    
+    public Genere(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Genere [nome=" + nome + ", libri=" + libri + "]";
+    }
+    
 }
