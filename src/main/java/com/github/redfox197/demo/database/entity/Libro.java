76f01e5c52c6bbccdfb1a360c846bff11b74e7c0
Sedex <giovanni.di.guida95@gmail.com>
2025-02-10 16:31:45 +0100

    Relazione manytomany

--- src/main/java/com/github/redfox197/demo/database/entity/Libro.java
+++ src/main/java/com/github/redfox197/demo/database/entity/Libro.java
@@ -1,11 +1,14 @@
 package com.github.redfox197.demo.database.entity;
 
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 
 
@@ -22,6 +25,9 @@ public class Libro {
 
     @Column(length = 64)
     private String IBSN;
+
+    @ManyToMany (mappedBy="libri")
+    private List<Genere> generi;
 
     @ManyToOne
     private Autore autore;
