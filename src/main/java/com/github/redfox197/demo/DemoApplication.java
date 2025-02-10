c5033e9c930582c6f9abd15867040dd85b42f52d
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-02-10 16:43:21 +0100

    base cli

--- src/main/java/com/github/redfox197/demo/DemoApplication.java
+++ src/main/java/com/github/redfox197/demo/DemoApplication.java
@@ -1,11 +1,23 @@
 package com.github.redfox197.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.redfox197.demo.cli.CliManager;
+import com.github.redfox197.demo.database.service.AutoreService;
+import com.github.redfox197.demo.database.service.GenereService;
+import com.github.redfox197.demo.database.service.LibroService;
+
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
+	@Autowired
+	private AutoreService autoreService;
+	@Autowired
+	private LibroService libroService;
+	@Autowired
+	private GenereService genereService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -13,7 +25,7 @@ public class DemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("hello world");
+		new CliManager(libroService, autoreService, genereService);
 	}
 
 }
--- src/main/java/com/github/redfox197/demo/cli/CliManager.java
+++ src/main/java/com/github/redfox197/demo/cli/CliManager.java
@@ -1,5 +1,23 @@
 package com.github.redfox197.demo.cli;
 
-public class CliManager {
+import com.github.redfox197.demo.database.service.AutoreService;
+import com.github.redfox197.demo.database.service.GenereService;
+import com.github.redfox197.demo.database.service.LibroService;
 
+public class CliManager {
+    private LibroService libroService;
+    private AutoreService autoreService;
+    private GenereService genereService;
+
+    public CliManager(LibroService libroService, AutoreService autoreService, GenereService genereService) {
+        this.libroService = libroService;
+        this.autoreService = autoreService;
+        this.genereService = genereService;
+
+        printOptions();
+    }
+
+    public void printOptions() {
+        System.out.println("Opzioni...");
+    }
 }
