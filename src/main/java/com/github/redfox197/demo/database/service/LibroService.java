b42faef8c17e780c86787b02e2b4ba25376e97a7
Sedex <giovanni.di.guida95@gmail.com>
2025-02-10 17:22:40 +0100

    Speriamo che abbiamo risolto

--- src/main/java/com/github/redfox197/demo/database/service/LibroService.java
+++ src/main/java/com/github/redfox197/demo/database/service/LibroService.java
@@ -1,5 +1,31 @@
 package com.github.redfox197.demo.database.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.github.redfox197.demo.database.entity.Libro;
+import com.github.redfox197.demo.database.repository.LibroRepo;
+
 public class LibroService {
+
+    @Autowired
+    LibroRepo libroRepo;
+
+    public List<Libro> findall(){
+        return libroRepo.findAll();
+    }
+
+    public void save(Libro libro){
+        libroRepo.save(libro);
+    }
+
+    public void delete(Libro libro){
+        libroRepo.delete(libro);
+    }
+
+    public Libro findbyid(Long id){
+        return libroRepo.findById(id).orElse(null);
+    }
 
 }
