36fc57a8e2f44a4c1aeca3c266375124e865301a
Dominic <118680299+bjdome@users.noreply.github.com>
2025-02-10 16:06:07 +0100

    implementazione autore

--- src/main/java/com/github/redfox197/demo/database/entity/Autore.java
+++ src/main/java/com/github/redfox197/demo/database/entity/Autore.java
@@ -1,5 +1,58 @@
 package com.github.redfox197.demo.database.entity;
 
-public class Autore {
+import java.util.ArrayList;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Autore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length=64)
+    private String nome;
+    @Column(length=128)
+    private String cognome;
+    @Column(length=2)
+    private String nazionalita;
+
+    @OneToMany(mappedBy="autore")
+    private ArrayList<Libro> libri;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public String getNazionalita() {
+        return nazionalita;
+    }
+    public void setNazionalita(String nazionalita) {
+        this.nazionalita = nazionalita;
+    }
+    public ArrayList<Libro> getLibri() {
+        return libri;
+    }
+    public void setLibri(ArrayList<Libro> libri) {
+        this.libri = libri;
+    }
 }
